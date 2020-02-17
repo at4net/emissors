c45f4493beaa9d8536f3e9ba288aabf6e3947756
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2020-02-17 10:22:16 +0100

    Tests

--- scsp-commons/utils/src/test/java/es/caib/scsp/utils/xml/DocumentsTest.java
+++ scsp-commons/utils/src/test/java/es/caib/scsp/utils/xml/DocumentsTest.java
@@ -15,19 +15,11 @@
  */
 package es.caib.scsp.utils.xml;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.annotation.XmlSchema;
-import javax.xml.parsers.ParserConfigurationException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
 
 /**
  *
@@ -60,9 +52,7 @@ public class DocumentsTest {
     @Test
     public void testStringToElement() {
         
-        String xml = "<datosEspecificos xmlns=\"http://intermediacion.redsara.es/scsp/esquemas/datosespecificos\"><Retorno><Estado><CodigoEstado>0</CodigoEstado><Literal>TRAMITADA</Literal></Estado><Consulta><Matricula>6201CXP</Matricula></Consulta><DatosVehiculo><DatosGenerales><DescripcionVehiculo><Bastidor>VF1BBR8EF31799191</Bastidor><Marca><Codigo>01551</Codigo><Descripcion>RENAULT</Descripcion></Marca><Modelo>CLIO</Modelo></DescripcionVehiculo><DomicilioVehiculoDGT><Municipio>CAMPANET</Municipio></DomicilioVehiculoDGT><DomicilioVehiculoINE><Municipio>CAMPANET</Municipio></DomicilioVehiculoINE><Matriculacion><FechaMatriculacion>2004-06-29</FechaMatriculacion><Matricula>6201CXP</Matricula></Matriculacion><Titular><DomicilioDGT><NombreVia>CAN LLIS POL 2 PARCELA 20</NombreVia><CodigoPostal>07310</CodigoPostal><Municipio>CAMPANET</Municipio><Provincia><Codigo>PM</Codigo><Descripcion>ILLES BALEARS</Descripcion></Provincia></DomicilioDGT></Titular></DatosGenerales><DatosTecnicos><NivelEmisiones>EURO 3A</NivelEmisiones><Masas><MasaMaximaTecnica>0</MasaMaximaTecnica><MasaServicio>0</MasaServicio><MasaMaxima>1540</MasaMaxima><Tara>995</Tara></Masas><Plazas><Mixtas>000</Mixtas><Normales>5</Normales><NumeroPlazasPie>0</NumeroPlazasPie></Plazas><Potencias><Cilindrada>1461.0</Cilindrada><PotenciaFiscal>11.02</PotenciaFiscal><PotenciaNetaMaxima>0.0</PotenciaNetaMaxima><RelacionPotenciaPeso>0.0</RelacionPotenciaPeso></Potencias></DatosTecnicos><DatosTramites><FechaTransferencia>2013-05-30</FechaTransferencia></DatosTramites></DatosVehiculo></Retorno></datosEspecificos>";
-        //String xml = "<datosEspecificos><Retorno><Estado><CodigoEstado>0</CodigoEstado><Literal>TRAMITADA</Literal></Estado><Consulta><Matricula>6201CXP</Matricula></Consulta><DatosVehiculo><DatosGenerales><DescripcionVehiculo><Bastidor>VF1BBR8EF31799191</Bastidor><Marca><Codigo>01551</Codigo><Descripcion>RENAULT</Descripcion></Marca><Modelo>CLIO</Modelo></DescripcionVehiculo><DomicilioVehiculoDGT><Municipio>CAMPANET</Municipio></DomicilioVehiculoDGT><DomicilioVehiculoINE><Municipio>CAMPANET</Municipio></DomicilioVehiculoINE><Matriculacion><FechaMatriculacion>2004-06-29</FechaMatriculacion><Matricula>6201CXP</Matricula></Matriculacion><Titular><DomicilioDGT><NombreVia>CAN LLIS POL 2 PARCELA 20</NombreVia><CodigoPostal>07310</CodigoPostal><Municipio>CAMPANET</Municipio><Provincia><Codigo>PM</Codigo><Descripcion>ILLES BALEARS</Descripcion></Provincia></DomicilioDGT></Titular></DatosGenerales><DatosTecnicos><NivelEmisiones>EURO 3A</NivelEmisiones><Masas><MasaMaximaTecnica>0</MasaMaximaTecnica><MasaServicio>0</MasaServicio><MasaMaxima>1540</MasaMaxima><Tara>995</Tara></Masas><Plazas><Mixtas>000</Mixtas><Normales>5</Normales><NumeroPlazasPie>0</NumeroPlazasPie></Plazas><Potencias><Cilindrada>1461.0</Cilindrada><PotenciaFiscal>11.02</PotenciaFiscal><PotenciaNetaMaxima>0.0</PotenciaNetaMaxima><RelacionPotenciaPeso>0.0</RelacionPotenciaPeso></Potencias></DatosTecnicos><DatosTramites><FechaTransferencia>2013-05-30</FechaTransferencia></DatosTramites></DatosVehiculo></Retorno></datosEspecificos>";
-        /*
+       /*
         String xml = "" +
 //                "<?xml version=\"1.0\"?>\n" +
 //"<!-- Edited by XMLSpy -->\n" +
@@ -84,6 +74,8 @@ public class DocumentsTest {
 "</catalog>";
         */
         
+      /*
+       
         System.out.println(xml);
         
         Element expResult = null;
@@ -97,41 +89,33 @@ public class DocumentsTest {
         } catch (IOException ex) {
             Logger.getLogger(DocumentsTest.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        */    
         
+       /*
        SVDDGTVEHICULODATOSWS01v3RespuestaDatosEspecificos datosEspecificos = null;
        
        System.out.println(SVDDGTVEHICULODATOSWS01v3RespuestaDatosEspecificos.class.getSuperclass().getPackage().getAnnotation(XmlSchema.class));
         
-       //datosEspecificos = JAXB.unmarshal(xml, SVDDGTVEHICULODATOSWS01v3RespuestaDatosEspecificos.class);
-        
        SVDDGTVEHICULODATOSWS01v3RespuestaDatosEspecificos respuesta = new SVDDGTVEHICULODATOSWS01v3RespuestaDatosEspecificos();
-       
-       
-       
-     
         
         try {
-            XmlManager<SVDDGTVEHICULODATOSWS01v3RespuestaDatosEspecificos> manager =
-                    new XmlManager<SVDDGTVEHICULODATOSWS01v3RespuestaDatosEspecificos>(
+            XmlManager<SVDDGTVEHICULODATOSWS01v3RespuestaDatosEspecificos> manager
+                    = new XmlManager<SVDDGTVEHICULODATOSWS01v3RespuestaDatosEspecificos>(
                             SVDDGTVEHICULODATOSWS01v3RespuestaDatosEspecificos.class
                     );
-            
+
             datosEspecificos = manager.generateItem(xml);
-            
+
             System.out.println(datosEspecificos);
-            
+
         } catch (JAXBException ex) {
             Logger.getLogger(DocumentsTest.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(DocumentsTest.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        
-        System.out.println(datosEspecificos.getRetorno());
-        
-        
-        System.out.println(result);
-        
+        */    
         
         //assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
